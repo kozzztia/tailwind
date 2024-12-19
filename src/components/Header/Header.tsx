17f47198263efadf4e90ec8dc87321a8a8629063
kozzztia <kozzztia@gmail.com>
2024-12-19 11:10:26 +0200

    initial

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header className="header w-full py-primaryPaddingY px-primaryPaddingX">
+      <h1 className="text-2xl font-bold">
+        Tailwind React TypeScript
+      </h1>
+    </header>
+  )
+}
+
+export default Header
