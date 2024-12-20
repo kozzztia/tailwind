b1150620d3762edff1ec067182220290743bd2b4
kozzztia <kozzztia@gmail.com>
2024-12-20 13:22:39 +0200

    finish profile card

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import CardContainer from "../ui-kit/Container/CardContainer";
 import { buttons, links } from "./const";
+import Wave from "./decore/wave.svg";
 
 const ProfileCard: React.FC<ProfileCardProps> = ({ name, className = "" }) => {
   return (
@@ -18,31 +20,37 @@ type ProfileCardProps = {
 
 const Card = () => {
   return (
-    <div className="w-[390px] h-[580px] flex flex-col bg-white rounded-[50px] overflow-hidden drop-shadow">
-      <div className="bg-[red] w-full h-[220px] flex justify-center items-start">
+    <div className="w-[390px] h-[580px] flex flex-col bg-white  rounded-[50px] overflow-hidden drop-shadow">
+      <div className="bg-inherit w-full h-[220px] flex justify-center items-start pt-10 relative">
         <img
           src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt="name"
-          className="w-[120px] h-[120px] rounded-full mt-4 object-cover drop-shadow"
+          className="w-[120px] h-[120px] rounded-full  object-cover drop-shadow relative z-10"
         />
+        <img
+          src={Wave}
+          alt="name"
+          className="w-full h-full absolute top-0 left-0 z-0 scale-150"
+        />
+
       </div>
       <article className="w-full flex flex-col justify-start items-center pt-5 px-9 pb-10 flex-grow">
-        <h4 className="w-full text-[29px] font-normal text-gray-600 text-center tracking-wider leading-2">
+        <h4 className="w-full text-[29px] font-normal text-black text-center tracking-wider leading-2">
           Johnny Rogers
         </h4>
         <a
           href="https://t.me/your_channel_name"
           target="_blank"
           rel="noopener noreferrer"
-          className="w-full text-[11px] font-normal text-gray-600 text-center hover:underline leading-1"
+          className="w-full text-[11px] font-normal text-gray-600 text-center leading-1"
         >
-          @telegram
+          @JohnnyRogers
         </a>
         <ul className="w-full flex justify-center items-center gap-6 mt-7">
           {links.map((item) => (
             <li
               key={item.id}
-              className="w-[25px] h-[25px] flex justify-center items-center rounded-full bg-[red]"
+              className="w-[25px] h-[25px] flex justify-center items-center rounded-full bg-inherit"
             >
               <a
                 href={item.url}
@@ -50,8 +58,9 @@ const Card = () => {
                 rel="noopener noreferrer"
                 className="w-full h-full flex items-center justify-center"
               >
-                {/* {item.icon} */}
-                icon
+                {React.createElement(item.icon, {
+                  className: "w-5 h-5 text-black transition duration-300 bg-inherit hover:text-[#d147a3] focus:text-[#d147a3] hover:scale-150 focus:scale-150"
+                })}
               </a>
             </li>
           ))}
@@ -64,15 +73,15 @@ const Card = () => {
         <nav className="w-full flex justify-between items-center gap-4 mt-auto">
           {
             buttons.map((item) => (
-              <button 
-              key={item.id}
-              onClick={item.action}
-              className="w-full text-[#b3b3cc] text-[14px] font-semibold py-[9px] px-4 rounded-full 
-              bg-inherit border border-[#b3b3cc] transition-colors duration-300
-              hover:bg-[#d147a3] hover:text-white focus:bg-[#d147a3] focus:text-white
+              <button
+                key={item.id}
+                onClick={item.action}
+                className="w-full text-[#b3b3cc] text-[14px] font-semibold py-[9px] px-4 rounded-full 
+              bg-inherit border border-[#b3b3cc] transition duration-300
+              hover:bg-[#d147a3] hover:text-white hover:border-[#d147a3] focus:bg-[#d147a3] focus:text-white focus:border-[#d147a3]
               ">
-              {item.name}
-            </button>
+                {item.name}
+              </button>
             ))
           }
 
