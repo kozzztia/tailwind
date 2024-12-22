2ce1d45346d8ee9d1e73dc3569b3840a847ddeab
kozzztia <kozzztia@gmail.com>
2024-12-22 16:36:50 +0200

    start generator

--- src/constants/list.ts
+++ src/constants/list.ts
@@ -1,29 +1,29 @@
 import { lazy } from 'react';
 
 export const list: ListItem[] = [
-    {
-        id: 1,
-        name: 'Profile Card',
-        component: lazy(() => import('../components/ProfileCard/ProfileCard')),
-        color: '#ffbf80'
-    },
-    {
-        id: 2,
-        name: 'Add To Bag challenge',
-        component: lazy(() => import('../components/AddToBagChallenge/AddToBagChallenge')),
-        color: '#fff'
-    },
-    {
-        id: 3,
-        name: 'Navigation',
-        component: lazy(() => import('../components/Navigation/Navigation')),
-        color: '#e6fff2'
-    },
+    // {
+    //     id: 1,
+    //     name: 'Profile Card',
+    //     component: lazy(() => import('../components/ProfileCard/ProfileCard')),
+    //     color: '#ffbf80'
+    // },
+    // {
+    //     id: 2,
+    //     name: 'Add To Bag challenge',
+    //     component: lazy(() => import('../components/AddToBagChallenge/AddToBagChallenge')),
+    //     color: '#fff'
+    // },
+    // {
+    //     id: 3,
+    //     name: 'Navigation',
+    //     component: lazy(() => import('../components/Navigation/Navigation')),
+    //     color: '#e6fff2'
+    // },
     {
         id: 4,
-        name: 'Profile Card',
-        component: lazy(() => import('../components/ProfileCard/ProfileCard')),
-        color: 'red'
+        name: 'Password generator',
+        component: lazy(() => import('../components/PasswordGenerator/PasswordGenerator')),
+        color: '#fff'
     }
 ]
 
