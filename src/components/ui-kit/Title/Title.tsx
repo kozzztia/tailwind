fc2f8152c0b9e36a615671dc3cdeb44c7d3e8426
kozzztia <kozzztia@gmail.com>
2024-12-22 09:15:52 +0200

    close/open button

--- src/components/ui-kit/Title/Title.tsx
+++ src/components/ui-kit/Title/Title.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Title: React.FC<TitleProps> = ({name, className="", Tag}) => {
+const Title: React.FC<TitleProps> = ({name, Tag}) => {
   return (
-    <Tag className={`text-2xl font-bold drop-shadow text-white text-left ${className}`}>{name}</Tag>
+    <Tag className={`text-2xl font-bold drop-shadow text-white text-left`}>{name}</Tag>
   )
 }
 
@@ -11,5 +11,4 @@ export default Title
 type TitleProps = {
   Tag: keyof JSX.IntrinsicElements; 
   name: string
-  className?: string
 }
