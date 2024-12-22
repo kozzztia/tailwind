dec443964b202b24ef3de842b6c642865c5949ab
kozzztia <kozzztia@gmail.com>
2024-12-22 16:37:56 +0200

    list enable

--- src/components/PasswordGenerator/PasswordGenerator.tsx
+++ src/components/PasswordGenerator/PasswordGenerator.tsx
@@ -4,6 +4,11 @@ const PasswordGenerator = () => {
         <div className="w-[340px] h-[566px] flex flex-col justify-start items-center rounded-[15px]
     px-[15px] py-[27px] bg-[#1a001a] shadow-sm shadow-black">
             <h4 className="w-full text-[20px] font-bold text-white">Generate password</h4>
+
+
+
+
+            
             <button 
             className="w-full h-[40px] rounded-[5px] bg-[#db62db] flex justify-center items-center
                 text-[12px] font-bold text-white mt-auto shadow-sm shadow-black tracking-wider
--- src/constants/list.ts
+++ src/constants/list.ts
@@ -1,24 +1,24 @@
 import { lazy } from 'react';
 
 export const list: ListItem[] = [
-    // {
-    //     id: 1,
-    //     name: 'Profile Card',
-    //     component: lazy(() => import('../components/ProfileCard/ProfileCard')),
-    //     color: '#ffbf80'
-    // },
-    // {
-    //     id: 2,
-    //     name: 'Add To Bag challenge',
-    //     component: lazy(() => import('../components/AddToBagChallenge/AddToBagChallenge')),
-    //     color: '#fff'
-    // },
-    // {
-    //     id: 3,
-    //     name: 'Navigation',
-    //     component: lazy(() => import('../components/Navigation/Navigation')),
-    //     color: '#e6fff2'
-    // },
+    {
+        id: 1,
+        name: 'Profile Card',
+        component: lazy(() => import('../components/ProfileCard/ProfileCard')),
+        color: '#ffbf80'
+    },
+    {
+        id: 2,
+        name: 'Add To Bag challenge',
+        component: lazy(() => import('../components/AddToBagChallenge/AddToBagChallenge')),
+        color: '#fff'
+    },
+    {
+        id: 3,
+        name: 'Navigation',
+        component: lazy(() => import('../components/Navigation/Navigation')),
+        color: '#e6fff2'
+    },
     {
         id: 4,
         name: 'Password generator',
