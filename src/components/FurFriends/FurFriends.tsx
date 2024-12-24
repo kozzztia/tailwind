8656d46cb9375b9052b496f5fcb5efc3015e1a4c
kozzztia <kozzztia@gmail.com>
2024-12-24 17:44:33 +0200

    friends logic

--- src/components/FurFriends/FurFriends.tsx
+++ src/components/FurFriends/FurFriends.tsx
@@ -1,8 +1,52 @@
+import { useState } from "react"
+import { FaDog } from "react-icons/fa6"
 
 const FurFriends = () => {
+  const [page, setPage] = useState<string>("home")
+  const [searchParam, setSearchParam] = useState<string>("")
+  const getHome = () => {
+    setPage("home")
+  }
   return (
-    <div className='w-[200px] h-[200px] bg-[red] rounded-sm flex justify-center items-center'>Fur friends</div>
+    <div
+      className='w-[235px] h-[480px] bg-[#fff] rounded-[10px] shadow-[0_0_40px_0] shadow-black overflow-clip 
+      px-[10px] pt-[15px] text-black
+      flex justify-start items-start
+      '>
+      {page === "home" ? <DogList />
+        : <DogPage name={"Bailey"} handler={getHome}/>
+    }
+      
+
+    </div>
   )
 }
 
 export default FurFriends
+
+const DogList = () => {
+  return (
+    <div className="w-full h-full flex flex-col justify-start items-start bg-[red]">
+      <h1 className="text-[14px] font-bold flex items-center gap-x-[5px]">
+        <span className="w-[20px] h-[20px] bg-black rounded-full flex justify-center items-center">
+          <FaDog className="w-[15px] h-[15px] text-white"/>
+        </span> 
+        FurEver Friends
+      </h1>
+    </div>
+  )
+}
+
+
+
+
+
+
+const DogPage: React.FC<{name: string, handler: () => void}> = ({name, handler}) => {
+  return (
+    <div>
+      <p>{name}</p>
+      <button onClick={handler}>Back</button>
+    </div>
+  )
+}
