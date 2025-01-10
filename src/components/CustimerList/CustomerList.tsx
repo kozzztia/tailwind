0b7c6e165d7fd62af3895a8b25a8047234b28981
kozzztia <kozzztia@gmail.com>
2025-01-11 01:49:31 +0200

    list structure

---
+++ src/components/CustimerList/CustomerList.tsx
@@ -0,0 +1,71 @@
+import style from "./style.module.css"
+import { Customer, mockCustomers } from "./const"
+import { useState } from "react"
+
+const CustomerList = () => {
+  return (
+    <div className={style.listContainer}>
+      <CustomerListHeader />
+
+      <List customers={mockCustomers} />
+    </div>
+  )
+}
+
+export default CustomerList
+
+const CustomerListHeader = () => {
+  return (
+    <header className={style.listHeader}>
+      <h2>Customer List</h2>
+      <button>
+        + Add Customer
+      </button>
+    </header>
+
+  )
+}
+
+const List: React.FC<{ customers: Customer[] }> = ({ customers }) => {
+  console.log(customers)
+  return (
+    <ul className={style.list}>
+      {customers.map((customer) => (
+        <ListItem key={customer.id} customer={customer} />
+      ))}
+    </ul>
+  )
+}
+
+const ListItem: React.FC<{ customer: Customer }> = ({ customer }) => {
+  const { name, email, phone, status, createdDate, image } = customer;
+  const [cheked, setChecked] = useState(false)
+  const checkHandler = () => {
+    setChecked(!cheked)
+  }
+  console.log(image)
+  return (
+    <li className={`${cheked && style.active} ${style.listItem}`}>
+      <button className={style.itemButton}
+        onClick={checkHandler}
+      >
+    V
+      </button>
+      <div className={style.avatar}>
+      {
+        image ? <img src={image} alt={name} />
+        : <div><p>{name.split(" ")[0][0]}{name.split(" ")[1][0]}</p></div>
+      }
+      </div>
+      <h3>{name}</h3>
+      <p>{email}</p>
+      <p>{phone}</p>
+      <p>{status}</p>
+      <p>{createdDate}</p>
+
+      <button className={style.itemButton}>
+    ...
+      </button>
+    </li>
+  )
+}
