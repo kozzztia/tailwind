169f8b85e10e252deb158cd77c6185eecf1cdcc7
kozzztia <kozzztia@gmail.com>
2025-01-17 02:29:28 +0200

    dashed + procent

--- src/components/ProgressBars/ProgressBars.tsx
+++ src/components/ProgressBars/ProgressBars.tsx
@@ -1,38 +1,50 @@
 
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css'
 
 const ProgressBars = () => {
+  const [progress, setProgress] = useState<number>(4);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => (prev < 6 ? prev + 1 : 0)); 
+    }, 500);
+
+    return () => clearInterval(interval); 
+  }, []);
+
+
+
   return (
     <div className={styles.container}>
 
-        <DashedProgressBar count={1} />
+      <DashedProgressBar count={1} progress={progress}/>
 
-        <div className={styles.progressBar}>
-          progrree
-        </div>
+      <ProcentProgressBar count={2} progress={progress}/>
 
-        <div className={styles.progressBar}>
-          progrree
-        </div>
+      <div className={styles.progressBar}>
+        progrree
+      </div>
 
-        <div className={styles.progressBar}>
-          progrree
-        </div>
-        <div className={styles.progressBar}>
-          progrree
-        </div>
+      <div className={styles.progressBar}>
+        progrree
+      </div>
 
-        <div className={styles.progressBar}>
-          progrree
-        </div>
+      <div className={styles.progressBar}>
+        progrree
+      </div>
 
-        <div className={styles.progressBar}>
-          progrree
-        </div>
-        
-        <div className={styles.progressBar}>
-          progrree
-        </div>
+      <div className={styles.progressBar}>
+        progrree
+      </div>
+
+      <div className={styles.progressBar}>
+        progrree
+      </div>
+
+      <div className={styles.progressBar}>
+        progrree
+      </div>
 
     </div>
   )
@@ -41,17 +53,17 @@ const ProgressBars = () => {
 export default ProgressBars;
 
 
-const ProgressBarContainer: React.FC<{count: number, children: React.ReactNode}> = ({count, children}) => {
+const ProgressBarContainer: React.FC<{ count: number, children: React.ReactNode, className?: string }> = ({ count, children, className }) => {
   return (
-    <div className={styles.progressBar}>
-      <Counter count={count}/>
+    <div className={[styles.progressBar, className].join(' ')}>
+      <Counter count={count} />
       {children}
     </div>
   )
 }
 
 
-const Counter: React.FC<{count: number}> = ({count}) => {
+const Counter: React.FC<{ count: number }> = ({ count }) => {
   return (
     <div className={styles.counter}>
       <span>{count}</span>
@@ -59,10 +71,35 @@ const Counter: React.FC<{count: number}> = ({count}) => {
   )
 }
 
-const DashedProgressBar: React.FC<{count: number}> = ({count}) => {
+const DashedProgressBar: React.FC<{ count: number, progress: number }> = ({ count, progress }) => {
+  const arr = [1, 2, 3, 4, 5, 6]
   return (
-    <ProgressBarContainer count={count}>
-      progrree
+    <ProgressBarContainer count={count} className={styles.dashedProgressBar}>
+      <div className={styles.line}>
+        {
+          arr.map((item) => (
+            <span key={item} className={`${styles.dashed} ${item <= progress ? styles.active : ''}`}>
+            </span>
+          ))
+        }
+      </div>
     </ProgressBarContainer>
   )
 }
+
+const ProcentProgressBar: React.FC<{ count: number; progress: number }> = ({ count, progress }) => {
+  const clampedProgress = Math.min(Math.max(100 * (progress / 6), 0));
+
+  return (
+    <ProgressBarContainer count={count} className={styles.procentProgressBar}>
+      <div
+        className={styles.line}
+        style={{ width: `${clampedProgress}%` }} 
+      ></div>
+    </ProgressBarContainer>
+  );
+};
+
+
+
+
